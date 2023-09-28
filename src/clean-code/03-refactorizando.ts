f0fd87fa508e1bf652cfd4d30150de5552d367e5
Gabriel Castillo <jesus.castilloc@outlook.com>
2023-09-28 10:42:02 -0300

    primer commit

---
+++ src/clean-code/03-refactorizando.ts
@@ -0,0 +1,8 @@
+const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
+  
+  if ( isDead ) return 1500;
+  
+  if ( isSeparated ) return 2500; 
+  
+  return  ( isRetired ) ? 3000 :  4000; 
+}
